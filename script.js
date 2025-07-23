396fa07acaa2336d257523672088326ac8502351
HARD-YWEB <tareainacap2025@gmail.com>
2025-07-23 02:28:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1085,11 +1085,11 @@ function submitQuiz() {
 
     let message = '';
     if (finalGrade < 4.0) {
-        message = "Yapo culiao ponele empeño, queri ganar plata o no? 😠💸";
+        message = "¡No te desanimes! Este es solo un paso en tu camino de aprendizaje. Cada intento te acerca más a dominar el contenido. ¡Con un poco más de estudio y práctica, verás grandes mejoras! ¡Tú puedes!😉💸";
     } else if (finalGrade >= 4.0 && finalGrade < 6.0) {
-        message = "Ya ta bien pero podi hacerlo mejor 😉👍";
+        message = "¡Felicidades! Has alcanzado la meta, ¡y eso es genial! Este resultado demuestra que tienes una buena base. Ahora, a seguir reforzando esos conocimientos para ir por más. ¡Vamos por ese 7! 😉👍";
     } else if (finalGrade >= 6.0 && finalGrade < 7.0) {
-        message = "Esooo, felicitaciones así se hace!! 🎉👏";
+        message = ""¡Wow, qué resultado tan fantástico! ¡Esto es una prueba de tu gran compromiso y que dominas el tema! ¡¡FELICITACIONES!!🎉👏";
     } else if (finalGrade == 7.0) {
         message = "FELICITACIONES RODRIGOOO ERES SECOOOO! 🔥🤩";
     }
